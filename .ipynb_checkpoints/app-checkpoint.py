5a50748ba5195acea02145c078c0551f63213dfc
leslybohu <l.b708995@gmail.com>
2019-04-07 21:27:14 -0500

    updated flask

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -10,11 +10,11 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-import pymysql
+from sqlalchemy import create_engine, func
+#import pymysql
 
 from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 
 
@@ -29,6 +29,10 @@ engine = create_engine("mysql+pymysql://root:banana@localhost/homes_db")
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
+
+print(dir(Base.classes))
+print(Base.classes.keys())
+print(Base.classes.items())
 
 # Save reference to the table
 home_table = Base.classes.home
@@ -54,39 +58,67 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/search")
+@app.route("/buyouts/search")
 def search():
-    """Return the list of homes in the database"""
+    return render_template("search.html")
+    
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+@app.route("/buyouts/analytics")
+def analytics():
+    return render_template("analytics.html")
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
-
-@app.route("/analytics")
-
-
-
-@app.route("/faqs")
+@app.route("/buyouts/faqs")
+def faqs():
+    return render_template("faqs.html")
 
 
-
-
-@app.route("/about")
+@app.route("/buyouts/alerts")
+def alerts():
+    return render_template("alerts.html")
 
 
 
-@app.route("/api/neighborhood/")
-#Send a list of houses in the neighborhood sorted by recency 
+#@app.route("/buyouts/charts")
+#def charts():
+    #return render_templates ("")
+
+
+@app.route("/buyouts/about")
+def about():
+    return render_template("about.html")
+
+
+
+#API routes below here 
+
+#need an api route for search
+#single neighborhood
+
+@app.route('/api/neighborhood', defaults={'neighborhood':True})
+@app.route("/api/neighborhood/<neighborhood>")
+def api(neighborhood):
+    print("neighborhood route")
+    results = session.query(home_table.Neighborhood,home_table.Address, home_table.Price, home_table.Days_on_Market, home_table.Agent).all()
+    return jsonify(results)
+   
+
+#change from array of arrays into an array of objects
+#query for a specific neighborhood instead of all rows 
 
 #Neighborhood Name
 #Address
 #Price
 #Days on Market
-#Agent
+#Agent    
+    
 
+#need an API route for analytics 
+
+@app.route("/api")
+def dummy():
+    return jsonify({
+        "test": "success"
+    })
 
 if __name__ == "__main__":
     app.run()
